f8214fe2924f5a4aef4c31229952f6cbb95a443d
Volker Theile <vtheile@suse.com>
2018-05-11 12:35:40 +0200

    Set 'ceph dashboard set-rgw-api-user-id <user>' in start-ceph.sh
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- start-ceph.sh
+++ start-ceph.sh
@@ -11,8 +11,6 @@ RGW=1 ../src/vstart.sh -d -n -x
 
 ./bin/radosgw-admin user create --uid=dev --display-name=Developer --system
 
-access_key=`./bin/radosgw-admin user info --uid=dev | jq .keys[0].access_key | sed -e 's/^"//' -e 's/"$//'`
-./bin/ceph dashboard set-rgw-api-access-key $access_key
-
-secret_key=`./bin/radosgw-admin user info --uid=dev | jq .keys[0].secret_key | sed -e 's/^"//' -e 's/"$//'`
-./bin/ceph dashboard set-rgw-api-secret-key $secret_key
+./bin/ceph dashboard set-rgw-api-user-id dev
+./bin/ceph dashboard set-rgw-api-access-key `./bin/radosgw-admin user info --uid=dev | jq .keys[0].access_key | sed -e 's/^"//' -e 's/"$//'`
+./bin/ceph dashboard set-rgw-api-secret-key `./bin/radosgw-admin user info --uid=dev | jq .keys[0].secret_key | sed -e 's/^"//' -e 's/"$//'`
