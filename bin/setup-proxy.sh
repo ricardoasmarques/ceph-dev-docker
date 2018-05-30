19d3abd77c94eb7f672f3a2f645854fe58480cc1
Ricardo Marques <rimarques@suse.com>
2018-05-30 11:35:07 +0100

    setup-proxy.sh should config `/ui-api`
    Signed-off-by: Ricardo Marques <rimarques@suse.com>

--- bin/setup-proxy.sh
+++ bin/setup-proxy.sh
@@ -6,4 +6,4 @@ cd /ceph/build
 url=`./bin/ceph mgr services | jq .dashboard`
 
 cd /ceph/src/pybind/mgr/dashboard/frontend
-jq '.["/api/"].target'=$url proxy.conf.json.sample > proxy.conf.json
+jq '.["/api/"].target'=$url proxy.conf.json.sample | jq '.["/ui-api/"].target'=$url > proxy.conf.json
