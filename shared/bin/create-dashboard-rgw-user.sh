0480f10c84e90c16573e8321cd6744e834e410e5
Lenz Grimmer <lenz@grimmer.com>
2019-02-07 14:10:31 +0100

    start-ceph.sh: split out RGW configuration
    Move dashboard RGW configuration into a
    separate file `create-dashboard-rgw-user.sh`
    in order to be able to run this step separately.
    
    Signed-off-by: Lenz Grimmer <lenz@grimmer.com>

---
+++ shared/bin/create-dashboard-rgw-user.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+#--------------
+# Configure RGW
+#--------------
+
+./bin/radosgw-admin user create --uid=dev --display-name=Developer --system
+./bin/ceph dashboard set-rgw-api-user-id dev
+./bin/ceph dashboard set-rgw-api-access-key `./bin/radosgw-admin user info --uid=dev | jq .keys[0].access_key | sed -e 's/^"//' -e 's/"$//'`
+./bin/ceph dashboard set-rgw-api-secret-key `./bin/radosgw-admin user info --uid=dev | jq .keys[0].secret_key | sed -e 's/^"//' -e 's/"$//'`
--- shared/bin/start-ceph.sh
+++ shared/bin/start-ceph.sh
@@ -8,13 +8,4 @@ cd /ceph/build
 RGW=1 ../src/vstart.sh -d -n -x
 
 setup-proxy.sh
-
-#--------------
-# Configure RGW
-#--------------
-
-./bin/radosgw-admin user create --uid=dev --display-name=Developer --system
-
-./bin/ceph dashboard set-rgw-api-user-id dev
-./bin/ceph dashboard set-rgw-api-access-key `./bin/radosgw-admin user info --uid=dev | jq .keys[0].access_key | sed -e 's/^"//' -e 's/"$//'`
-./bin/ceph dashboard set-rgw-api-secret-key `./bin/radosgw-admin user info --uid=dev | jq .keys[0].secret_key | sed -e 's/^"//' -e 's/"$//'`
+create-dashboard-rgw-user.sh
