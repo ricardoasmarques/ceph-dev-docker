4717ba194f2aa60510d79a45de23d566fd665f62
Ricardo Marques <rimarques@suse.com>
2019-04-29 10:26:11 +0100

    Fix RGW user creation script
    Signed-off-by: Ricardo Marques <rimarques@suse.com>

--- shared/bin/create-dashboard-rgw-user.sh
+++ shared/bin/create-dashboard-rgw-user.sh
@@ -6,6 +6,7 @@ set -e
 # Configure RGW
 #--------------
 
+cd /ceph/build
 ./bin/radosgw-admin user create --uid=dev --display-name=Developer --system
 ./bin/ceph dashboard set-rgw-api-user-id dev
 ./bin/ceph dashboard set-rgw-api-access-key `./bin/radosgw-admin user info --uid=dev | jq -r ".keys[0].access_key"`
