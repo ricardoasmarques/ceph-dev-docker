0ecb008446b1d344a3329aa63663e2cc98f5731d
Ricardo Marques <rimarques@suse.com>
2019-06-27 16:02:13 +0100

    Fix keyring path
    Signed-off-by: Ricardo Marques <rimarques@suse.com>

--- shared/bin/start-ceph.sh
+++ shared/bin/start-ceph.sh
@@ -7,7 +7,7 @@ find /ceph/build/ -name "mgr.*.log" -type f -delete
 cd /ceph/build
 RGW=1 ../src/vstart.sh -d -n -x
 
-chmod +r keyring
+chmod +r /ceph/build/keyring
 
 setup-proxy.sh
 create-dashboard-rgw-user.sh
