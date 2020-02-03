d2f168a4bae137a4e4a5becd51ce6512ecdfd81d
Patrick Seidensal <pseidensal@suse.com>
2020-02-03 11:50:31 +0100

    add temporary fix for `stray hosts/services` of `orchestrator_cli`
    Signed-off-by: Patrick Seidensal <pseidensal@suse.com>

--- shared/bin/start-ceph.sh
+++ shared/bin/start-ceph.sh
@@ -12,5 +12,10 @@ chmod +r /ceph/build/keyring
 ./bin/ceph config set mgr mgr/dashboard/log_level info
 ./bin/ceph config set mgr mgr/dashboard/log_to_file true
 
+# Temporary fix for `stray hosts/services` issue of `orchestrator_cli`
+./bin/ceph config set mgr mgr/cephadm/warn_on_stray_hosts false && \
+    ceph config set mgr mgr/cephadm/warn_on_stray_services false && \
+    ceph mgr module enable cephadm
+
 setup-proxy.sh
 create-dashboard-rgw-user.sh
